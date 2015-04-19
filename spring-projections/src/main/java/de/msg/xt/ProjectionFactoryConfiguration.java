d3b4bdc4e115211e39b9ec567833a51dd570971c
Michael Schaefer <Michael.Schaefer@msg-systems.com>
2015-04-19 09:45:57 +0200

    introduce CRUD-Service based on MVC-Controller with manually mapping of URI {id} and projection connection

--- spring-projections/src/main/java/de/msg/xt/ProjectionFactoryConfiguration.java
+++ spring-projections/src/main/java/de/msg/xt/ProjectionFactoryConfiguration.java
@@ -2,6 +2,7 @@ package de.msg.xt;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.data.projection.SpelAwareProxyProjectionFactory;
 
--- spring-projections/src/main/java/de/msg/xt/route/web/RouteController.java
+++ spring-projections/src/main/java/de/msg/xt/route/web/RouteController.java
@@ -4,12 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.projection.ProjectionFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import de.msg.xt.route.Route;
 import de.msg.xt.route.RouteRepository;
 import de.msg.xt.route.projection.ConnectionProjection;
+
 
 @RestController
 @RequestMapping("/mvc/routes")
@@ -18,22 +21,18 @@ public class RouteController {
 	@Autowired
 	private RouteRepository routeRepository;
 	
-	private ProjectionFactory projectionFactory;
-	
 	@Autowired
-	public RouteController(ProjectionFactory projectionFactory) {
-		this.projectionFactory=projectionFactory;
-	}
-	
-	
-	@RequestMapping
-	public ResponseEntity<ConnectionProjection> getAll() {
+	private ProjectionFactory projectionFactory;
+		
+		
+	@RequestMapping("{id}/connection")
+	public ResponseEntity<ConnectionProjection> get(@PathVariable(value="id") Long id) {
 			
-		Iterable<Route> routes = routeRepository.findAll();
+		Route routes = routeRepository.findOne( new Long(id));
 		
 		ConnectionProjection connectionProjection = projectionFactory.createProjection(ConnectionProjection.class, routes);
 		return new ResponseEntity<ConnectionProjection>(connectionProjection,HttpStatus.OK);
-				
+		
 		
 	}
 	
